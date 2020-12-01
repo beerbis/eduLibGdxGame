62d2ed47f71031e9340a035b49a354edf2b4826b
Виктор Елсуков <beerbis@yandex.ru>
2020-12-02 02:09:58 +0300

    Initial commit

---
+++ android/src/ru/beerbis/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.beerbis;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.beerbis.SatanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SatanGame(), config);
+	}
+}
---
+++ desktop/src/ru/beerbis/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.beerbis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.beerbis.SatanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SatanGame(), config);
+	}
+}
